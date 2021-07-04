f9b9bb832957395a378eb3f00ce098447963a39a
Kris Hopper <hopperdavis@gmail.com>
2021-07-04 09:12:59 -0700

    add fetch method, UI Logic, html framework

--- src/currency-exchange.js
+++ src/currency-exchange.js
@@ -1,5 +1,15 @@
 export default class CurrencyExchange {
-  static getRates(code) {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+  static getRates(target, amount) {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${target}/${amount}`)
+      .then(function (response) {
+        console.log(response);
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function (error) {
+        return error;
+      });
   }
 }
--- src/main.js
+++ src/main.js
@@ -2,9 +2,25 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyExchange from './currency-exchange.js';
 
 
+$(document).ready(function () {
+  $('form#currencyEx').submit(function () {
+    event.preventDefault();
+    let target = $('#targetCurrency').val();
+    let amount = $('#amount').val();
 
-$(document).ready(function() {
-  
+    CurrencyExchange.getRates(target, amount)
+      .then(function (response) {
+        console.log("THIS IS WORKING");
+        getElements(response);
+      });
+  });
 });
+
+function getElements(response) {
+  if (response.result === "success")
+    return response.conversion_rate;
+  console.log("THIS WAS A GOOD FUNCITON" + response.conversion_rate);
+}
