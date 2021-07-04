60c4f62d36227fcc490efc21edeeed23dc1a3dec
Kris Hopper <hopperdavis@gmail.com>
2021-07-04 16:25:51 -0700

    add show/hide for results and errors

--- src/main.js
+++ src/main.js
@@ -9,14 +9,20 @@ function getElements(response) {
   let target = $('#target').val();
   let amount = $('#amount').val();
 
-  if (response.result === "success") {
-    $('#showAmount').text(`${amount} ${code} is ${response.conversion_result} ${target}`);   
+  if (response.result === "success") {   
+    $('.showAmount').text(`${amount} ${code} is ${response.conversion_result} ${target}`);  
+    $('#results').show();
+    $('#error').hide();
   } else if (response["error-type"] === "unsupported-code")  {
-    $('#showError').text(`${target} is an unsupported currency code, or the currency does not exist.`);
+    $('.showError').text(`${target} is an unsupported currency code, or the currency does not exist.`);
+    $('#error').show();
+    $('#results').hide();
   } else {
-    $('#showError').text(`This is an invalid search: ${response.message}`);
+    $('.showError').text(`This is an invalid search: ${response.message}`);
+    $('#error').show();
+    $('#results').hide();
   }
-  
+
   $('form#currencyEx').trigger("reset");
 }
 
@@ -26,8 +32,7 @@ $(document).ready(function () {
     let code = $('#code').val();
     let target = $('#target').val();
     let amount = $('#amount').val();
-    $('#results').show();
-
+    
     CurrencyExchange.getRates(code, target, amount)
       .then(function (response) {
         getElements(response);
